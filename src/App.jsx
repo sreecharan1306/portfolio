5576f345ef168ee169eba642af82fa70da3bd06b
sreecharan1306 <mscharan1313@gmail.com>
2025-03-16 19:37:03 +0530

    some useless changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,15 @@
-import { OrbitControls, Sparkles } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { useRef } from "react";
-import { useFrame } from "@react-three/fiber";
-
-const RotatingCube = () => {
-  const meshRef = useRef();
-  useFrame(() => {
-    if (meshRef.current) {
-      meshRef.current.rotation.y += 0.01;
-      meshRef.current.rotation.x += 0.01;
-      meshRef.current.rotation.z += 0.01;
-    }
-  })
-  return (
-    <mesh ref={meshRef}>
-      <cylinderGeometry args={[1, 1, 1]} />
-      <meshLambertMaterial color={"#468585"} emissive={"#468585"} />
-      <Sparkles count={330} scale={2} size={3} speed={0.1} noise={0.1} color={"#red"}/>
-    </mesh>
-  );
-};
+import React from "react";
+import "./app.css"
+import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
 
 const App = () => {
   return (
-    <Canvas
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <OrbitControls enableZoom enablePan enableRotate />
-      <directionalLight position={[1, 1, 1]} intensity={10} color={"#9CDBA6"} />
-      <color attach="background" args={["#000000"]} />
+    <div className="max-w-7xl mx-auto">
+      <Navbar />
+      <Hero />
+    </div>
+  )
+}
 
-      <RotatingCube />
-    </Canvas>
-  );
-};
 export default App;
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+
+const Hero = () =>{
+    return (
+        <section id="home" className="min-h-screen border-2 border-blue-500 flex flex-col relative">
+            <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 gap-3 sm:px-10 px-5">
+                
+            </div>
+        </section>
+    )
+
+}
+
+export default Hero;
